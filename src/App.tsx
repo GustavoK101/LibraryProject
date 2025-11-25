763bdc286e0fcb91dcb921736d02c72b8b44ea58
GustavoK101 <gustavokramer123@gmail.com>
2025-11-25 15:59:09 -0300

    Feat: added SearchBooksPage and SearchBook component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar'; 
 import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { Homepage } from './layouts/Homepage/Homepage';
+import { Homepage } from './layouts/HomePage/Homepage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () =>{
   return (
     <div>
       <Navbar />
-      <Homepage />
+      {/*HomePage*/}
+      <SearchBooksPage />
       <Footer/>
     </div>
   );
