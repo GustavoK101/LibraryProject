d04d50edd9a73fddb477ae8b90a484a613696495
GustavoK101 <gustavokramer123@gmail.com>
2025-11-27 23:17:42 -0300

    fix: fixed links to search page and home

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -2,6 +2,7 @@ import { ReturnBook } from "./ReturnBook";
 import BookModel from "../../../Models/BookModel";
 import { useEffect, useState } from "react";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 
 export const Carousel = () => {
@@ -131,9 +132,9 @@ export const Carousel = () => {
         </div>
       </div>
       <div className="homepage-carousel-title mt-3 ">
-        <a className="btn btn-outline-secondary btn-lg" href="#">
+        <Link className="btn btn-outline-secondary btn-lg" to="/search">
           View More
-        </a>
+        </Link>
       </div>
     </div>
   );
